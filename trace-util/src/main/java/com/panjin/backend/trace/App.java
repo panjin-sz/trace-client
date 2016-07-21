5077785a47bf29295abd51d590cd894d7ccabebc
panjin <panjin@myhome.163.com>
2016-07-21 15:24:16 +0800

    build project

---
+++ trace-util/src/main/java/com/panjin/backend/trace/App.java
@@ -0,0 +1,13 @@
+package com.panjin.backend.trace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
